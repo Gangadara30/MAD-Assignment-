4fe25b0f297861d829164e8f50a1287a8e719285
wasu-he <wasuu.herath@gmail.com>
2021-01-13 00:02:33 +0530

    merge conflict fixes

--- app/src/main/java/com/example/bookworm/ConnectPubCus.java
+++ app/src/main/java/com/example/bookworm/ConnectPubCus.java
@@ -1,11 +1,11 @@
 package com.example.bookworm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ConnectPubCus extends AppCompatActivity {
     private Button button;
