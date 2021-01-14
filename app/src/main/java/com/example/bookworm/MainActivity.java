1346071c9238bb1bb9174eead5a4e68dd42ebe20
wasu-he <wasuu.herath@gmail.com>
2021-01-14 22:27:44 +0530

    merge conflict resolved

--- app/src/main/java/com/example/bookworm/MainActivity.java
+++ app/src/main/java/com/example/bookworm/MainActivity.java
@@ -1,18 +1,16 @@
  package com.example.bookworm;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -32,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_customer);
         listView=findViewById(R.id.booklist);
 
         reference = FirebaseDatabase.getInstance().getReference().child("Book_Details");
