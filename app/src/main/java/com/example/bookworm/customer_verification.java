8fe90df3d4ee897602e357fca99dc4fa41b03ad5
wasu-he <wasuu.herath@gmail.com>
2021-01-14 21:44:12 +0530

    merge conflict resolved

--- app/src/main/java/com/example/bookworm/customer_verification.java
+++ app/src/main/java/com/example/bookworm/customer_verification.java
@@ -20,7 +20,7 @@ public class customer_verification extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_otp);
 
 
 
